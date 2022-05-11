2897a12f32436667558e5b5158b84e495d559a1a
ibisodee <ibiso.dokubo.t@gmail.com>
2022-05-11 19:38:24 +1000

    New May

--- src/Pages/Services.js
+++ src/Pages/Services.js
@@ -14,7 +14,7 @@ function services () {
       <div>
         <div className='w-5/6 max-w-5/6 m-auto py-10'>
           <h2 className='font-bold my-5 text-blue text-2xl'>Our Key Services</h2>
-          <p className='mb-5 text-justify'>Okon Solar Enegy is One of African’s leading trusted suppliers and integrators of renewable energy products. We are an Australian owned and operated company. Okon Solar was formed to help residences and commences overcome the biggest challenges faced because of high electricity/utility bills. Our aim is to help both commercial enterprises and residences access energysaving equipment with minimal fuss. We have been offering complete renewable solutions, tailored to our individual customer requirements.,</p>
+          <p className='mb-5 text-justify'>Okon Solar Enegy is One of African’s leading trusted suppliers and integrators of renewable energy products. We are a Nigerian owned and operated company. Okon Solar was formed to help residences and commences overcome the biggest challenges faced because of high electricity/utility bills. Our aim is to help both commercial enterprises and residences access energysaving equipment with minimal fuss. We have been offering complete renewable solutions, tailored to our individual customer requirements.,</p>
           <img src='images/services.jpg' alt='Ranges of Services' />
           <div className='md:grid md:grid-flow-row-dense grid-cols-2  py-10 gap-4'>
             <div className=''>
--- src/components/Intro.js
+++ src/components/Intro.js
@@ -11,7 +11,7 @@ function intro () {
           </div>
           <div className='p-5'>
             <h2 className='font-bold my-5 text-blue lg:text-2xl md:text-lg sm:text-md'>OUR COMPANY OKON SOLAR ENERGY</h2>
-            <p className='mb-5 text-justify'>Okon Solar Enegy is One of African’s leading trusted suppliers and integrators of renewable energy products. We are an Australian owned and operated company. Okon Solar was formed to help residences and commences overcome the biggest challenges faced because of high electricity/utility bills. Our aim is to help both commercial enterprises and residences access energysaving equipment with minimal fuss. We have been offering complete renewable solutions, tailored to our individual customer requirements.,</p>
+            <p className='mb-5 text-justify'>Okon Solar Enegy is One of African’s leading trusted suppliers and integrators of renewable energy products. We are a Nigerian owned and operated company. Okon Solar was formed to help residences and commences overcome the biggest challenges faced because of high electricity/utility bills. Our aim is to help both commercial enterprises and residences access energysaving equipment with minimal fuss. We have been offering complete renewable solutions, tailored to our individual customer requirements.,</p>
             <button className='px-3 py-1 bg-blue text-white hover:bg-blue md:text-sm sm:text-xs hover:animate-pulse hover:text-yellow rounded-sm'>READ MORE INFORMATION ABOUT US</button>
           </div>
         </div>
